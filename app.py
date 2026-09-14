d9e7e66cadc571c67765cdfe3bac1101077dbc6f
Mpok-java <mariocoelho0510@gamil.com>
2026-09-14 18:20:32 -0300

    update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
@@ -14,31 +15,18 @@ def sobre_o_sistema():
 
 @app.route('/aluno')
 def listar_aluno():
-    lista_aluno = [
-        (1, 'João', 20, 'São Paulo'),
-        (2, 'Maria', 22, 'Rio de Janeiro'),
-        (3, 'Pedro', 19, 'Belo Horizonte'),
-        (4, 'Ana', 21, 'Teresina'),
-        (5, 'Carlos', 23, 'Parnaíba'),
-        (6, 'Juliana', 20, 'Picos'),
-        (7, 'Lucas', 18, 'Floriano'),
-        (8, 'Mariana', 22, 'Campo Maior'),
-        (9, 'Gabriel', 19, 'Oeiras'),
-        (10, 'Beatriz', 21, 'Piripiri'),
-        (11, 'Rafael', 20, 'São Raimundo Nonato'),
-        (12, 'Camila', 23, 'Barras'),
-        (13, 'Felipe', 18, 'Esperantina'),
-        (14, 'Larissa', 22, 'Altos'),
-        (15, 'Bruno', 21, 'Pedro II'),
-        (16, 'Amanda', 20, 'União'),
-        (17, 'Daniel', 19, 'José de Freitas'),
-        (18, 'Isabela', 23, 'Bom Jesus'),
-        (19, 'Matheus', 18, 'Uruçuí'),
-        (20, 'Letícia', 21, 'Corrente'),
-        (21, 'Gustavo', 20, 'Imperatriz'),
-        (22, 'Sofia', 22, 'São Luís')
-    ]
-    return render_template('aluno/lista.html', listar_alunos =lista_aluno)
+
+    DB_PATH = "banco_escola.db"
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT id, nome, idade, cidade FROM aluno')
+
+    lista = cursor.fetchall()
+
+    conn.close()
+    
+    return render_template('aluno/lista.html', lista =lista)
 
 
 @app.route('/professor')
